4e418d91193a6f658c3d92db50bc77de253ca9e7
linostar <linux.anas@gmail.com>
2015-09-01 14:40:45 +0300

    Added setup.py

---
+++ JSOV/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+	name = 'JSOV',
+	packages = ['JSOV'],
+	version = '0.1',
+	description = 'JSON visualization lib based on JSOV or custom templates',
+	author = 'Anas El Husseini',
+	author_email = 'linux.anas@gmail.com',
+	url = 'https://github.com/linostar/pypi-JSOV',
+	download_url = 'https://github.com/linostar/pypi-JSOV/tarball/0.1',
+	keywords = ['JSON', 'JSOV', 'templates', 'visualization'],
+	classifiers = [],
+)
