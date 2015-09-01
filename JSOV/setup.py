a2d5b10982fc0025b94830887e879583e9c7fbe5
linostar <linux.anas@gmail.com>
2015-09-01 15:03:07 +0300

    Adjusted version number

--- JSOV/setup.py
+++ JSOV/setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
 	name = 'JSOV',
 	packages = ['JSOV'],
-	version = '0.1',
+	version = '0.1.1',
 	description = 'JSON visualization lib based on JSOV or custom templates',
 	author = 'Anas El Husseini',
 	author_email = 'linux.anas@gmail.com',
 	license = 'Revised BSD',
 	url = 'https://github.com/linostar/pypi-JSOV',
-	download_url = 'https://github.com/linostar/pypi-JSOV/tarball/0.1',
+	download_url = 'https://github.com/linostar/pypi-JSOV/tarball/0.1.1',
 	keywords = ['JSON', 'JSOV', 'templates', 'visualization'],
 	install_requires = ['PyYAML>=3.11', 'cssutils>=1.0', 'dpath>=1.4.0'],
 	classifiers = [
