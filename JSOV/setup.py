3d789c939a4a66ba7503f8bb45e61c5da957d59c
linostar <linux.anas@gmail.com>
2015-09-01 14:58:52 +0300

    Updated setup.py with missing entries

--- JSOV/setup.py
+++ JSOV/setup.py
@@ -6,8 +6,16 @@ setup(
 	description = 'JSON visualization lib based on JSOV or custom templates',
 	author = 'Anas El Husseini',
 	author_email = 'linux.anas@gmail.com',
+	license = 'Revised BSD',
 	url = 'https://github.com/linostar/pypi-JSOV',
 	download_url = 'https://github.com/linostar/pypi-JSOV/tarball/0.1',
 	keywords = ['JSON', 'JSOV', 'templates', 'visualization'],
-	classifiers = [],
+	install_requires = ['PyYAML>=3.11', 'cssutils>=1.0', 'dpath>=1.4.0'],
+	classifiers = [
+	'Development Status :: 3 - Alpha',
+	'Intended Audience :: Developers',
+	'Topic :: Utilities',
+	'License :: OSI Approved :: BSD License',
+	'Programming Language :: Python :: 3',
+	],
 )
